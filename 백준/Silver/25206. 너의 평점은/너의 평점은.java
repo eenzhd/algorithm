1c10cc3e416bbaf9208a805edc30fc2e942551f3
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-04 21:20:11 +0900

    [Silver V] Title: 너의 평점은, Time: 192 ms, Memory: 18112 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,51 +1,53 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		double sum = 0;   // 학점 * 과목평점  
-		double hsum = 0;   // 학점의 총합  
-		
-		for(int i=0; i < 20; i++) {
-			String s = sc.next();   // 과목  
-			double h = sc.nextDouble();   // 학점  
-			String g = sc.next(); 
-			
-			if(g.equals("A+")) {
-				sum += 4.5 * h ;
-				hsum += h;
-			} else if(g.equals("A0")) {
-				sum += 4.0 * h;
-				hsum += h;
-			} else if(g.equals("B+")) {
-				sum += 3.5 * h;
-				hsum += h;
-			} else if(g.equals("B0")) {
-				sum += 3.0 * h;
-				hsum += h;
-			} else if(g.equals("C+")) {
-				sum += 2.5 * h;
-				hsum += h;
-			} else if(g.equals("C0")) {
-				sum += 2.0 * h;
-				hsum += h;
-			} else if(g.equals("D+")) {
-				sum += 1.5 * h;
-				hsum += h;
-			} else if(g.equals("D0")) {
-				sum += 1.0 * h; 
-				hsum += h;
-			} else if(g.equals("F")) {
-				sum += 0.0 * h;
-				hsum += h;
-			} 
-		
-		}
-		
-		System.out.printf("%.6f",sum/hsum);
-		
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float sum = 0.0f;
+        float scoreSum = 0.0f;
+        for (int i = 0; i < 20; i++) {
+            String subjects = sc.next();
+            float score = sc.nextFloat();
+            String grade = sc.next();
+            switch(grade) {
+                case "A+" :
+                    sum += score * 4.5f;
+                    scoreSum += score;
+                    break;
+                case "A0" :
+                    sum += score * 4.0f;
+                    scoreSum += score;
+                    break;
+                case "B+" :
+                    sum += score * 3.5f;
+                    scoreSum += score;
+                    break;
+                case "B0" :
+                    sum += score * 3.0f;
+                    scoreSum += score;
+                    break;
+                case "C+" :
+                    sum += score * 2.5f;
+                    scoreSum += score;
+                    break;
+                case "C0" :
+                    sum += score * 2.0f;
+                    scoreSum += score;
+                    break;
+                case "D+" :
+                    sum += score * 1.5f;
+                    scoreSum += score;
+                    break;
+                case "D0" :
+                    sum += score * 1.0f;
+                    scoreSum += score;
+                    break;
+                case "F" :
+                    sum += score * 0.0f;
+                    scoreSum += score;
+                    break;
+            }
+        }
+        System.out.println(sum / scoreSum);
+    }
 }
