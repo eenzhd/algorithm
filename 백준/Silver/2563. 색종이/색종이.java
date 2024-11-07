3524d5ae5bc312986bc112355ee738bfa2724087
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-07 22:49:29 +0900

    [Silver V] Title: 색종이, Time: 180 ms, Memory: 17936 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,29 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main (String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int paper = sc.nextInt();
-		int width = 0;  // 넓이
-		boolean [][] bArr = new boolean[101][101];  // 도화지
-		// boolean 배열을 선언하고 어떤 수에 대해 bArr[x][y]가 true면
-		// 그 부분은 색종이가 붙여져 있는 것이다.
-		
-		for(int i=0; i<paper; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			//(x,y) 부터 (x+9, y+9)까지 영역을 하나씩 체크한 후 총 넓이에 더해준다.
-			for(int j=x; j<x+10; j++) {
-				for(int k=y; k<y+10; k++) {
-					if(bArr[j][k]!=true) {
-						bArr[j][k]=true;
-						width++;
-					}
-				}
-			}
-			
-		}
-		System.out.println(width);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[][] arr = new int[100][100];
+        int width = 0;
+        for (int i = 0; i < num; i++) {
+            int left = sc.nextInt();
+           
+            int bottom = sc.nextInt();
+            for (int j = left; j < 10 + left; j++) {
+                for (int k = bottom; k < 10 + bottom; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 1) {
+                    width++;
+                }
+            }
+        }
+        System.out.println(width);
+    }
 }
