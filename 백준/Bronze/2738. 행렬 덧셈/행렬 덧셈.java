b3da22c818937073f1998ab50646bdbb50ee43e4
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-07 22:37:01 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 524 ms, Memory: 37128 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,31 +1,26 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[][] aArr = new int[n][m];
-        int[][] bArr = new int[n][m];
-
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                aArr[i][j] = sc.nextInt();
+                arr1[i][j] = sc.nextInt();
             }
         }
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                bArr[i][j] = sc.nextInt();
+                arr2[i][j] = sc.nextInt();
             }
         }
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.print(aArr[i][j] + bArr[i][j] + " ");
+                System.out.print(arr1[i][j] + arr2[i][j] + " ");
             }
             System.out.println();
         }
