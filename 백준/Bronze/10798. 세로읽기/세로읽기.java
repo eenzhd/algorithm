019ff697666ecc8e3ef52a34b553d9dbab5d4a9c
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-08 23:00:07 +0900

    [Bronze I] Title: 세로읽기, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[][] input = new char[5][15];
+        char[][] arr = new char[5][15];
 
-        for (int i = 0; i < 5; i++) {
-            String str = sc.next();
+        for (int i = 0; i < arr.length; i++) {
+            String str = sc.nextLine();
             for (int j = 0; j < str.length(); j++) {
-                input[i][j] = str.charAt(j);
+                arr[i][j] = str.charAt(j);
             }
         }
-
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
-                if (input[j][i] != '\0') { // null 문자 
-                    System.out.print(input[j][i]);
+                if (arr[j][i] != '\0') {
+                    System.out.print(arr[j][i]);
                 }
             }
         }
