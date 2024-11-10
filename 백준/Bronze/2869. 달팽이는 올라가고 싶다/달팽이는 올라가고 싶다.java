1ac6d74b474512593e353dc83452656905e70d1d
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-10 19:08:03 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 180 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,21 +1,16 @@
-
 import java.util.Scanner;
 
 public class Main {
-	public static void main (String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int up = sc.nextInt();
-		int down = sc.nextInt();
-		int length = sc.nextInt();
-		int snail = 0;
-		int day = (length-down) /(up-down);
-		
-		if((length-down)%(up-down)!=0) {
-			snail++;
-		}
-		
-				System.out.println(day + snail);
-			
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();  // 낮에 올라가는 높이
+        int b = sc.nextInt();  // 밤에 미끄러지는 높이
+        int v = sc.nextInt();  // 나무막대의 높이
+
+        int day = (v - b) / (a - b);  // 마지막 날 내려가는 것 제외
+        if ((v - b) % (a - b) != 0) {  // 하루가 더 필요
+            day++;
+        }
+        System.out.println(day);
+    }
 }
