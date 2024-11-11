2f0ee53b517f24579a622ae4329ac22add50e6ab
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-11 23:16:38 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 264 ms, Memory: 18452 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int t = sc.nextInt();
         int[] dollar = {25, 10, 5, 1};
-
         for (int i = 0; i < t; i++) {
-            int cents = sc.nextInt();
-            int remainder = cents;
+            int money = sc.nextInt();
             for (int j = 0; j < dollar.length; j++) {
-                System.out.print(remainder / dollar[j] + " ");
-                remainder = remainder % dollar[j];
+                System.out.print(money / dollar[j] + " ");
+                money = money % dollar[j];
             }
             System.out.println();
         }
