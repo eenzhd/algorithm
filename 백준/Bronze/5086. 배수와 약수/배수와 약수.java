c3891bba673134d9838961df0e659aa906805f6e
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-12 21:56:20 +0900

    [Bronze III] Title: 배수와 약수, Time: 168 ms, Memory: 17532 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         while (true) {
             int num1 = sc.nextInt();
             int num2 = sc.nextInt();
-
-            if (num1 == 0 || num2 == 0) {
+            if (num1 == 0 && num2 == 0) {
                 break;
             }
-
             if (num2 % num1 == 0) {
                 System.out.println("factor");
-            } else if (num1 % num2 == 0) {
+            } else if(num1 % num2 == 0) {
                 System.out.println("multiple");
-            } else if (num2 % num1 != 0 && num1 % num2 != 0) {
+            } else {
                 System.out.println("neither");
             }
         }
