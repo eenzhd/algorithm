b60e375b1df2e2e0b4384324caec25d3cd0b0fcd
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-13 22:13:13 +0900

    [Bronze III] Title: 약수 구하기, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        List<Integer> arr = new ArrayList<Integer>();
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int count = 0;
-        int result = 0;
-
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
-                count++;
-                if (count == k) {
-                    result = i;
-                    break;
-                }
+                arr.add(i);
             }
         }
-        System.out.println(result);
+        Collections.sort(arr);
+        if (arr.size() < k) {
+            System.out.println(0);
+        } else {
+            System.out.println(arr.get(k - 1));
+        }
     }
 }
