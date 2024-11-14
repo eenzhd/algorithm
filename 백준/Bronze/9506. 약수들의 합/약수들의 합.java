7e4be27b34203fa47e9b73c7998438d8b747a8bd
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-14 22:22:29 +0900

    [Bronze I] Title: 약수들의 합, Time: 200 ms, Memory: 18384 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,30 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         while (true) {
             int num = sc.nextInt();
             if (num == -1) {
                 break;
             }
-
+            List<Integer> list = new ArrayList<>();
             int sum = 0;
-            StringBuilder sb = new StringBuilder();
-
             for (int i = 1; i < num; i++) {
-                if (num % i == 0){
+                if (num % i == 0) {
+                    list.add(i);
                     sum += i;
-                    sb.append(i).append(" + ");
                 }
             }
-
-            if (sum == num) {
-                sb.setLength(sb.length() - 3);
-                System.out.println(num + " = " + sb);
-            } else {
+            Collections.sort(list);
+            if (sum != num) {
                 System.out.println(num + " is NOT perfect.");
+            } else {
+                System.out.print(num + " = ");
+                for (int j = 0; j < list.size(); j++) {
+                    if (j == list.size() - 1) {
+                        System.out.println(list.get(j));
+                    } else {
+                        System.out.print(list.get(j) + " + ");
+                    }
+                }
             }
         }
     }
