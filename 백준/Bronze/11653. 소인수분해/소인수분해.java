540cd055018267349a2f388687e8571fe4abdaaa
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-17 16:25:45 +0900

    [Bronze I] Title: 소인수분해, Time: 208 ms, Memory: 17824 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 2; i <= n ;) {
-            if (n > 1) {
-                if (n % i == 0) {
+        int num = sc.nextInt();
+        for (int i = 2; i <= num; ) {
+            if (num > 1) {
+                if (num % i == 0) {
+                    num /= i;
                     System.out.println(i);
-                    n = n / i;
                 } else {
                     i++;
                 }
