b86c1022dbbfb7375c4a6c761314f8301d15407d
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-19 22:29:03 +0900

    [Bronze II] Title: 진법 변환, Time: 176 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -3,12 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String b = sc.next();
+        String str = sc.next();
         int num = sc.nextInt();
-
-        sc.close();
-
-        System.out.println(Integer.valueOf(b, num));
-
+        System.out.println(Integer.valueOf(str, num));
     }
 }
