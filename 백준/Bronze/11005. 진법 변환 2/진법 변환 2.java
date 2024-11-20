e98a8554049b09856c1b4b10d8f664d23b3b876e
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-20 21:51:52 +0900

    [Bronze I] Title: 진법 변환 2, Time: 180 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println(Integer.toString(n, b).toUpperCase());
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(Integer.toString(num1, num2).toUpperCase());
     }
 }
