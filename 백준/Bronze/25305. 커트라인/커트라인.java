6d078c5a1163a065139953f2208568dac5b54759
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-11-24 17:33:54 +0900

    [Bronze II] Title: 커트라인, Time: 244 ms, Memory: 20996 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -1,16 +1,27 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] score = new int[n];
-        for (int i = 0; i < n; i++) {
-            score[i] = sc.nextInt();
+        int num = sc.nextInt();  // 응시자 수
+        int k = sc.nextInt();  // 상을 받는 사람의 수
+
+        int[] arr = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
         }
-        Arrays.sort(score);
-        System.out.println(score[n - k]);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i; j < arr.length - 1; j++) {
+                int tmp = 0;
+                if (arr[i] < arr[j + 1]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println(arr[k - 1]);
     }
 }
