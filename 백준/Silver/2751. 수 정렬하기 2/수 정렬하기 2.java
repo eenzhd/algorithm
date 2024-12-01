62179a95af58a6e359fb5f05badaabc9e093b479
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-12-01 15:34:48 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1504 ms, Memory: 188228 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,29 +1,25 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.valueOf(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<Integer>();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             list.add(Integer.valueOf(st.nextToken()));
         }
         Collections.sort(list);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int num : list) {
-            bw.write(num + "\n");
+        for (int j = 0; j < list.size(); j++) {
+            bw.write(list.get(j) + " ");
         }
-        br.close();
         bw.flush();
         bw.close();
+        br.close();
     }
 }
