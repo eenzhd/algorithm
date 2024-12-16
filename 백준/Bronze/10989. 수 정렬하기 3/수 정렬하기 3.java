deb1ea49698874fec84cb065afd936c093bdb3f2
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-12-16 21:33:44 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1652 ms, Memory: 301420 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.valueOf(br.readLine());
-        int[] counting = new int[10001];
+        int[] count = new int[10001];
         for (int i = 0; i < n; i++) {
-            counting[Integer.valueOf(br.readLine())]++;
+            int num = Integer.valueOf(br.readLine());
+            count[num]++;
         }
-        for (int j = 1; j <= 10000; j++) {
-            while (counting[j] > 0) {
-                bw.write(j + "\n");
-                counting[j]--;
+        for (int i = 0; i <= 10000; i++) {
+            while (count[i] > 0) {
+                bw.write(i + "\n");
+                count[i]--;
             }
         }
-        br.close();
         bw.flush();
         bw.close();
     }
