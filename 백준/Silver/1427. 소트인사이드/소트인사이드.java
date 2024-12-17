50a2cc255b3fb5016d3c927abcab39e196d2167a
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-12-17 21:39:41 +0900

    [Silver V] Title: 소트인사이드, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,22 +1,18 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
-
-        while (n > 0) {
-            list.add(n % 10);
-            n /= 10;
+        int num = sc.nextInt();
+        int[] count = new int[10];
+        while(num > 0) {
+            count[num % 10]++;
+            num /= 10;
         }
-        Collections.sort(list);
-
-        for (int i = 1; i <= list.size(); i++) {
-            System.out.print(list.get(list.size() - i));
+        for (int j = count.length - 1; j >= 0; j--) {
+            while (count[j]-- > 0) {
+                System.out.print(j);
+            }
         }
     }
 }
