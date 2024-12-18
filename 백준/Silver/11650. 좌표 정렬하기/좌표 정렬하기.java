fd5e9313dc05f43b9e6b8e402f18fcd7d1964b72
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-12-18 20:32:22 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2420 ms, Memory: 199908 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,27 +1,33 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
-        int[][] arr = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().trim().split(" ");
-            arr[i][0] = Integer.parseInt(input[0]);
-            arr[i][1] = Integer.parseInt(input[1]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[][] arr = new int[num][2];
+        for (int i = 0; i < num; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
         }
-        Arrays.sort(arr, (arr1, arr2) -> {
-            if (arr1[0] == arr2[0]) {
-                return Integer.compare(arr1[1], arr2[1]);
+
+        /* 람다식으로 Comparator 정의
+            a와 b는 arr의 요소로 Arrays.sort가 a, b를 비교하여 정렬 순서를 결정함.
+         */
+        Arrays.sort(arr, (a, b) -> { // 람다식으로 Comparator 정의
+            // a[0]과 b[0]이 같으면 y좌표를 기준으로 정렬
+            if (a[0] == b[0]) {
+                return a[1] - b[1]; // 음수: a[1], 양수: b[1]
+            } else {
+                return a[0] - b[0];  // 음수 a[0], 양수: b[0]
             }
-            return Integer.compare(arr1[0], arr2[0]);
         });
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i][0] + " " + arr[i][1]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
-        br.close();
     }
 }
