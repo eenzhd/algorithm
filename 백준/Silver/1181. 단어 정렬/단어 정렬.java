b1cccf65dc30a357ebb49e61c17d259bd35f31e2
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-12-23 22:00:08 +0900

    [Silver V] Title: 단어 정렬, Time: 744 ms, Memory: 41000 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,34 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] strArr = new String[n];
-        for (int i = 0; i < n; i++) {
-            strArr[i] = sc.next();
-        }
-
+        int num = sc.nextInt();
         Set<String> set = new HashSet<>();
-        for (int j = 0; j < strArr.length; j++) {
-            set.add(strArr[j]);
+        for (int i = 0; i < num; i++) {
+            set.add(sc.next());
         }
-        List<String> list = new ArrayList<>(set);
-
+        List<String> list = new ArrayList<>(set.stream().collect(Collectors.toList()));
         Collections.sort(list, (a, b) -> {
-            if (a.length() != b.length()) {
-                return a.length() - b.length();
-            } else {
+            if (a.length() == b.length()) {
                 return a.compareTo(b);
+            } else {
+                return a.length() - b.length();
             }
         });
 
-        for (String str : list) {
+        for(String str : list) {
             System.out.println(str);
         }
     }
