928319cf2760132e91adde0d44150c0d83654a83
Heesoo <120344687+eenzhd@users.noreply.github.com>
2024-12-28 23:07:47 +0900

    [Bronze II] Title: 벌집, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int num = sc.nextInt();
-        int honey = 1;  // 현재 범위의 마지막 방(1, 7, 19 ...)
-        int count = 1;  // 방 카운트
-        int increase = 6;  // 지나야 하는 방의 수(6의 배수)
-
-        while (num > honey) {
-            honey += increase;
-            increase += 6;
+        int room = 1;
+        int count = 1;
+        while (room < num) {
+            room += 6 * count;
             count++;
         }
-
         System.out.println(count);
     }
 }
