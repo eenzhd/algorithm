61aaef58de34bfb13e864760fd278597ecc698e6
Heesoo <120344687+eenzhd@users.noreply.github.com>
2025-01-20 22:42:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        if (n < 5) {
+            if (n == 3) {
+                System.out.println(1);
+            } else {
+                System.out.println(-1);
+            }
+            return;
+        }
+
+        Arrays.fill(dp, 9999);
+
+        dp[3] = 1;
+        dp[5] = 1;
+
+        for (int i = 6; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
+        }
+
+        if (dp[n] > 9999) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[n]);
+        }
+    }
+}
