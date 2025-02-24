291199453c4f3c5a29882a9f03eafa960344edb6
Heesoo <120344687+eenzhd@users.noreply.github.com>
2025-02-24 20:56:31 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 168 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = fibonacci(n);
+        System.out.println(sum);
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+}
