c7ab63fed295e4750a9fa58e26c8f948014553c0
Heesoo <120344687+eenzhd@users.noreply.github.com>
2025-03-17 21:36:39 +0900

    [Bronze III] Title: 팩토리얼, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int answer = factorial(n);
+        System.out.println(answer);
+    }
+    public static int factorial(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
